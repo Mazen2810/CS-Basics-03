67c72e3d1f20668a1fb20626b82421c1b254203c
Mazen2810 <mazenmuhammed52@gmail.com>
2024-12-20 17:45:36 +0200

    Value Type Casting

--- CS-Basics-03/Program.cs
+++ CS-Basics-03/Program.cs
@@ -3,6 +3,29 @@ internal class Program
 {
      static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        #region Value type Casting
+        #region Implicit Casting
+        //int X = 4; // 4 byte
+        //long Y = /*(long)*/ X;
+        #endregion
+
+        #region Explict Casting
+        //long X = 35234324234234; // 8 byte
+        //int Y;
+        //checked
+        //{
+        //    Y = (int)X;
+        //}
+        //Console.WriteLine(Y);
+
+        //int x = 88;
+        //double y = /*(double)*/x;
+
+        //double x = 88.8;
+        //int y =(int) x;
+        //Console.WriteLine(y);
+        #endregion
+        #endregion
+
     }
 }
