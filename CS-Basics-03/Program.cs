321090824d7fa6a9e8b1a37a30880be761aae954
Mazen2810 <mazenmuhammed52@gmail.com>
2024-12-20 18:26:29 +0200

    Parse & Convert

--- CS-Basics-03/Program.cs
+++ CS-Basics-03/Program.cs
@@ -25,6 +25,40 @@ internal class Program
         //int y =(int) x;
         //Console.WriteLine(y);
         #endregion
+
+        #region Parse [Convert from string to any datatype]
+        //int Number = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Enter Data USer");
+
+        //Console.Write("Enter Name : ");
+        //String Name = Console.ReadLine();
+
+        //Console.Write("Enter Age : ");
+        //int Age = int.Parse(Console.ReadLine());
+
+        //Console.Write("Enter Salary : ");
+        //double Salary = double.Parse(Console.ReadLine());
+
+        //Console.Clear();
+        //Console.WriteLine("Name : "+Name+" Age : "+Age+" Salary : "+Salary);
+
+        #endregion
+        #region Convert [Convert from any datatype to any datatype]
+        //Console.WriteLine("Enter Data USer");
+
+        //Console.Write("Enter Name : ");
+        //String Name = Console.ReadLine();
+
+        //Console.Write("Enter Age : ");
+        //int Age = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write("Enter Salary : ");
+        //double Salary = Convert.ToDouble(Console.ReadLine());
+
+        //Console.Clear();
+        //Console.WriteLine("Name : " + Name + " Age : " + Age + " Salary : " + Salary);
+        #endregion
         #endregion
 
     }
